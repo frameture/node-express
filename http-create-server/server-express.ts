76dc77e12498871f2e42627778cb2333456e2442
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-27 17:50:04 +0200

    using Morgan logger (express middleware)

--- http-create-server/server-express.ts
+++ http-create-server/server-express.ts
@@ -1,19 +1,28 @@
 import * as express from 'express';
 import * as http from 'http';
+import * as logger from 'morgan';
 
 export default function start(): void {
   const app = express();
 
-  app.use(logger);
+  app.use(logger('short'));
+  app.use(authenticate);
   app.use((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end('<h1>express server response</h1>');
   });
+
   http.createServer(app)
     .listen(3001, () => console.log('express server listening on 3001'));
 
-  function logger(req, res, next): void {
-    console.log('New request from ' + req.url);
+  function authenticate(req, res, next): void {
+    const minute = new Date().getMinutes();
+    if ((minute % 2)) {
+      res.statusCode = 403;
+      res.end('Not authorized');
+      return;
+    }
     next();
   }
+
 }
